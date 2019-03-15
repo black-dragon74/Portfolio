a3b3ba2554960d21f16c0d7365781e531dc2d17e
black.dragon74 <nickk.2974@gmail.com>
2019-03-16 01:51:14 +0530

    A bit more ready
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

---
+++ src/components/Contact.vue
@@ -0,0 +1,172 @@
+<template>
+    <section id="contact">
+        <v-layout
+        pa-5
+        wrap>
+            <!-- Header flex -->
+            <v-flex xs12>
+                <h2 class="display-2 font-weight-bold primary--text mb-4">Let's talk</h2>
+            </v-flex>
+
+            <!-- Sub desc flex -->
+            <v-flex xs12 md5 mb-4 pr-5>
+                <h2 class="primary--text title font-weight-light text-uppercase mb-2">What can I do for you?</h2>
+                <div class="subheading mb-5">
+                    Have anything that you would like to say to me? Well you are more than welcome to do so. Please fill out the form below and hit that send button or else get in touch using the details given along with the form.
+                </div>
+                <h2 class="primary--text title font-weight-light text-uppercase mb-3">Send me a message</h2>
+                <v-form
+                ref="form"
+                method ="post"
+                lazy-validation
+                v-model="formIsValid"
+                autocomplete="off"
+                autofill="off"
+                >
+                    <v-layout wrap>
+                        <v-flex xs12>
+                            <v-text-field
+                            label="Name"
+                            flat
+                            solo
+                            :rules="nonEmptyRule"
+                            required
+                            ></v-text-field>
+                        </v-flex>
+
+                        <v-flex xs12>
+                            <v-text-field
+                            label="Email"
+                            flat
+                            solo
+                            :rules="emailRules"
+                            required
+                            ></v-text-field>
+                        </v-flex>
+
+                        <v-flex xs12>
+                            <v-text-field
+                            label="Subject"
+                            :rules="nonEmptyRule"
+                            flat
+                            solo
+                            required
+                            ></v-text-field>
+                        </v-flex>
+
+                        <v-flex xs12>
+                            <v-textarea
+                            solo
+                            flat
+                            :rules="nonEmptyRule"
+                            label="Message"
+                            required
+                            ></v-textarea>
+                        </v-flex>
+
+                        <v-flex text-xs-right>
+                            <v-btn
+                            color="primary"
+                            type="submit"
+                            ref="submitBtn"
+                            :disabled="!formIsValid">
+                                Send <v-icon class="ml-2">mdi-send</v-icon>
+                            </v-btn>
+                        </v-flex>
+                    </v-layout>
+                </v-form>
+            </v-flex>
+
+            <!-- The contact info block -->
+            <v-flex
+            xs12 md3 text-xs-left>
+                <h2 class="primary--text title font-weight-light text-uppercase">Social Contacts</h2>
+
+                <!-- Social Links -->
+                <div class="my-4">
+                    <a href="#" class="mx-2"
+                    v-for="(icon, i) in socialIcons"
+                    :title="icon.name"
+                    :key=i>
+                        <v-icon>{{ icon.icon }}</v-icon>
+                    </a>
+                </div>
+
+                <!-- The list with details -->
+                <v-list class="transparent" three-line>
+                    <v-list-tile
+                    avatar
+                    v-for="(det, i) in contactDetails"
+                    :key=i>
+                        <v-list-tile-avatar>
+                            <v-icon
+                            color="primary"
+                            class="mr-5"
+                            style="font-size: 48px;"
+                            >{{ det.icon }}</v-icon>
+                        </v-list-tile-avatar>
+
+                        <v-list-tile-content>
+                            <v-list-tile-title class="mr-2 primary--text">{{ det.title }}</v-list-tile-title>
+
+                            <v-list-tile-sub-title>
+                                {{ det.val }}
+                            </v-list-tile-sub-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-flex>
+
+            <!-- The image block -->
+            <v-flex
+            xs12 md4>
+                <v-img contain style="top: 95px;"
+                src="https://material-portfolio-dark.vuetifyjs.com/img/john-alt.7fef353c.png"></v-img>
+            </v-flex>
+        </v-layout>
+    </section>
+</template>
+
+
+<script>
+export default {
+    data () {
+        return {
+            formIsValid: true,
+
+            nonEmptyRule: [
+                v => !!v || "This field can't be empty"
+            ],
+
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+/.test(v) || 'E-mail must be valid'
+            ],
+
+            contactDetails: [
+                {"title": "Address", "val": "Jaipur, RJ, India", "icon": "mdi-map-marker"},
+                {"title": "Phone", "val": "+91-8302716971", "icon": "mdi-phone"},
+                {"title": "Email", "val": "me@nicksuniversum.com", "icon": "mdi-email"}
+            ],
+
+            socialIcons: [
+                {"name": "Facebook", "link": "https://fb.me/ydvnick", "icon": "mdi-facebook"},
+                {"name": "Github", "link": "https://github.com/black-dragon74", "icon": "mdi-github-circle"},
+                {"name": "Telegram", "link": "https://t.me/ydvnick", "icon": "mdi-telegram"},
+            ]
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+    section {
+        position: relative;
+    }
+
+    a {
+        text-decoration: none;
+    }
+</style>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,166 @@
+<template>
+    <section id="portfolio">
+        <v-layout
+        pa-5
+        wrap>
+            <!--  That abstract image -->
+            <v-img
+            :src="require('@/assets/abstract-3.png')"
+            class="abstract-3"
+            height=100%
+            width=100%
+            ></v-img>
+
+            <!--  The header -->
+            <h2 class="display-2 font-weight-bold primary--text mb-4">What I've done</h2>
+
+            <!-- The carousel for the rest of the part of this page -->
+            <v-carousel
+            class="elevation-0"
+            height=600
+            hide-controls
+            :cycle=false>
+                <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                >
+                    <v-sheet
+                    class="transparent pa-5"
+                    height=100%>
+                        <!-- The caoursel content is contained in the sheet -->
+                        <div class="text-xs-center mb-5">
+                            <h2 class="primary--text title font-weight-light mb-1">Advanced Library Management</h2>
+                            <span class="subheading">Created for fun</span>
+                        </div>
+
+                        <!-- The description of the current project -->
+                        <v-layout
+                        wrap>
+                            <!-- Flex 1 -->
+                            <v-flex shrink>
+                                <div class="mb-3" style="max-width: 500px;">
+                                    <!-- Details -->
+                                    <h2 class="primary--text title font-weight-light mb-3">Project Details</h2>
+
+                                    <div class="subheading mb-5">
+                                        {{ item.desc }}
+                                    </div>
+
+                                    <!-- Info -->
+                                    <h2 class="primary--text title font-weight-light">Project Info</h2>
+                                    <v-list class="transparent">
+                                        <v-list-tile>
+                                            <v-list-tile-action>
+                                                <v-icon color="primary">mdi-code-tags</v-icon> 
+                                            </v-list-tile-action>
+                                            <v-list-tile-title class="mr-2">{{ item.lang }}</v-list-tile-title>
+                                        </v-list-tile>
+
+                                        <v-list-tile>
+                                            <v-list-tile-action>
+                                                <v-icon color="primary">mdi-github-box</v-icon> 
+                                            </v-list-tile-action>
+                                            <v-list-tile-title class="mr-2">
+                                                <a :href="item.source" class="white--text">External Link <v-icon>mdi-link-variant</v-icon></a>
+                                            </v-list-tile-title>
+                                        </v-list-tile>
+
+                                        <v-list-tile>
+                                            <v-list-tile-action>
+                                                <v-icon color="primary">mdi-calendar</v-icon> 
+                                            </v-list-tile-action>
+                                            <v-list-tile-title class="mr-2">{{ item.year }}</v-list-tile-title>
+                                        </v-list-tile>
+                                    </v-list>
+                                </div>
+                            </v-flex>
+
+                            <!-- The flex that will be hidden on small and down -->
+                            <v-flex hidden-sm-and-down>
+                                <v-img
+                                height=350
+                                width=500
+                                contain
+                                :src="item.img"
+                                class="mx-auto"></v-img>
+                            </v-flex>
+                        </v-layout>
+                    </v-sheet>
+                </v-carousel-item>
+            </v-carousel>
+        </v-layout>
+    </section>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    desc: "A library management system written in JAVA. Uses MySQL for the database management. All the data is encrypted using BCrypt. This was my first big project and was coded by me in 7th Grade.",
+                    lang: "JAVA",
+                    source: "abc.zyx",
+                    year: "2010",
+                    img: require('@/assets/hero.png')
+                },
+
+                {
+                    desc: "A library management system written in JAVA. Uses MySQL for the database management. All the data is encrypted using BCrypt. This was my first big project and was coded by me in 7th Grade.",
+                    lang: "JAVA",
+                    source: "abc.zyx",
+                    year: "2010",
+                    img: ""
+                },
+
+                {
+                    desc: "A library management system written in JAVA. Uses MySQL for the database management. All the data is encrypted using BCrypt. This was my first big project and was coded by me in 7th Grade.",
+                    lang: "JAVA",
+                    source: "abc.zyx",
+                    year: "2010",
+                    img: ""
+                },
+
+                {
+                    desc: "A library management system written in JAVA. Uses MySQL for the database management. All the data is encrypted using BCrypt. This was my first big project and was coded by me in 7th Grade.",
+                    lang: "JAVA",
+                    source: "abc.zyx",
+                    year: "2010",
+                    img: ""
+                },
+
+                {
+                    desc: "A library management system written in JAVA. Uses MySQL for the database management. All the data is encrypted using BCrypt. This was my first big project and was coded by me in 7th Grade.",
+                    lang: "JAVA",
+                    source: "abc.zyx",
+                    year: "2010",
+                    img: ""
+                },
+            ]
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    section {
+        position: relative;
+    }
+
+    .abstract-3 {
+        position: absolute !important;
+        left: 0;
+        right: 0;
+        z-index: 0 !important;
+    }
+    
+    .abstract-3~* {
+        z-index: 1 !important;
+    }
+
+    a {
+        text-decoration: none;
+    }
+</style>
